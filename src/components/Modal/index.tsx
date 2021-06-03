0fb3f4290d5191f47c7288f4c27370a96642608d
Thales Testoni <thalestestoni@gmail.com>
2021-06-03 18:42:05 -0300

    Refactor modal component to use typescript

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react';
+import ReactModal from 'react-modal';
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  children: ReactNode;
+}
+
+export default function Modal({ isOpen, setIsOpen, children }: ModalProps) {
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={() => setIsOpen(false)}
+      isOpen={isOpen}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
